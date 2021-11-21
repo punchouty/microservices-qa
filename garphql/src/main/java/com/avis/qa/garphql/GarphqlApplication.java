0ac4aa90844df67290c02d98d5861aab37e80167
Rajan Punchouty <rajan@punchouty.com>
2021-11-21 18:04:37 +0530

    Added graphql projecy

---
+++ garphql/src/main/java/com/avis/qa/garphql/GarphqlApplication.java
@@ -0,0 +1,13 @@
+package com.avis.qa.garphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarphqlApplication.class, args);
+    }
+
+}
