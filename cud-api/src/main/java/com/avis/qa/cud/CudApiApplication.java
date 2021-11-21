a71f019de1c91bc6e359fe81433d8f5f847c173f
Rajan Punchouty <rajan@punchouty.com>
2021-11-21 18:11:56 +0530

    Added cud, kafka producer project

---
+++ cud-api/src/main/java/com/avis/qa/cud/CudApiApplication.java
@@ -0,0 +1,13 @@
+package com.avis.qa.cud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CudApiApplication.class, args);
+    }
+
+}
---
+++ cud-api/src/test/java/com/avis/qa/cud/CudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avis.qa.cud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
