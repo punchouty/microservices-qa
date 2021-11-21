a96b625b6a85d1a3240275b2d8df551fbeab7df8
Rajan Punchouty <rajan@punchouty.com>
2021-11-21 18:14:28 +0530

    Added orchestration api

---
+++ orchestration/src/test/java/com/avis/qa/orchestration/OrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avis.qa.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
