4f727f960da8faf3d2e409806b53db8f7e897284
Rajan Punchouty <rajan@punchouty.com>
2021-11-22 16:08:33 +0530

    Integrated with PostgreSQL

--- cud-api/src/main/java/com/avis/qa/cud/domain/Brand.java
+++ cud-api/src/main/java/com/avis/qa/cud/domain/Brand.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
@@ -16,6 +17,8 @@ public class Brand {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique=true)
+    @NotBlank(message = "Brand name is mandatory")
     private String name;
 
     @OneToMany(mappedBy = "brand")
--- cud-api/src/main/java/com/avis/qa/cud/domain/Location.java
+++ cud-api/src/main/java/com/avis/qa/cud/domain/Location.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -16,6 +17,8 @@ public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique=true)
+    @NotBlank(message = "Location name is mandatory")
     private String name;
     @ManyToOne
     @JoinColumn(name="brand_id")
