8bdf3d669c835743d8249ab3f590873691e99eb6
Rajan Punchouty <rajan@punchouty.com>
2021-11-22 18:50:49 +0530

    Graphql test module added

---
+++ graphql-test/src/test/java/com/avis/qa/graphql/test/GraphqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avis.qa.graphql.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
