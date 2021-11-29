8cd4bdf7e4f7bad7e73e7aa8951d69b15505e09d
Rajan Punchouty <rajan@punchouty.com>
2021-11-30 02:30:05 +0530

    Added project to test orchestration service

---
+++ orchestration-test/src/main/java/com/avis/qa/orchestration/test/OrchestrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.avis.qa.orchestration.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrchestrationTestApplication.class, args);
+    }
+
+}
---
+++ orchestration-test/src/test/java/com/avis/qa/orchestration/test/OrchestrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avis.qa.orchestration.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
